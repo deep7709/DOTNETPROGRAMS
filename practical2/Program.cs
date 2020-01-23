f2d317c544aa3ea13b24ffb20da9d9ba2fc5bb07
Deep Fuletra <17comp.deeep.fuletra@gmail.com>
2020-01-23 09:11:38 +0530

    final practical 1 2

---
+++ practical2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace practical2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string first_name, country;
+            while (true)
+            {
+                Console.WriteLine("Enter your name:");
+                first_name = Console.ReadLine();
+                Console.WriteLine("where are you from?");
+                country = Console.ReadLine();
+
+                if (!(Regex.IsMatch(first_name, @"^[a-z A-Z]+$") && Regex.IsMatch(country, @"^[a-z A-Z]+$")))
+                {
+                    Console.WriteLine("NOT PROPER FORMAT,ONLY CHARCTOR ARE ALLOWED IS THE FORMAT\n");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Hello" + first_name + "from" + country);
+                    break;
+                }
+            }
+            Console.Read();
+        }
+    }
+}
