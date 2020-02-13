bf548a08047a6130e739793808959b9c9f213ec9
Deep Fuletra <17comp.deeep.fuletra@gmail.com>
2020-02-13 09:11:33 +0530

    practical 4

---
+++ practical4/ClassA.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practical4
+{
+    class ClassA
+    {
+
+        public void infixtopostfix(string infix, out string postfix)
+        {
+            char ch;
+            postfix = "";
+            Stack<char> a = new Stack<char>();
+
+            for (int i = 0; i < infix.Length; i++)
+            {
+                ch = infix[i];
+
+                if (isOperator(ch))
+                {
+                    if (a.Count == 0)
+                    {
+                        a.Push(ch);
+                    }
+
+                    else
+                    {
+
+                        if (ch == '(')
+                        {
+                            a.Push(ch);
+                        }
+
+                        else if (ch == ')')
+                        {
+                            while (a.Peek() != '(')
+                            {
+                                postfix += a.Pop();
+                            }
+                            a.Pop();
+                       
+                        }
+
+                        else
+                        {
+
+                            if (priority(ch) > priority(a.Peek()))
+                            {
+                                a.Push(ch);
+                            }
+                            else
+                            {
+                                postfix += a.Pop();
+                                i--;
+                            }
+
+                        }
+
+                    }
+
+                }
+
+                else
+                {
+                    postfix += ch;
+                }
+            }
+            foreach (var item in a)
+                postfix += item;
+        }
+        private static int priority(char ch)
+        {
+            switch (ch)
+            {
+                case '*':
+                    return 2;
+
+                case '+':
+                    return 1;
+
+                case '-':
+                    return 1;
+
+                case '/':
+                    return 2;
+
+                case '(':
+                    return 0;
+
+                default:
+                    return 5;
+            }
+        }
+
+        private bool isOperator(char ch)
+        {
+            if (ch == '+' || ch == '-' || ch == '*' || ch == '/' || ch == '(' || ch == ')') 
+                  return true;
+            return false;
+        }
+    }
+}
---
+++ practical4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace practical4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter The Infix");
+            string infix = "a+b";
+            ClassA obj = new ClassA();
+            string postfix = " ";
+            obj.infixtopostfix(infix ,out postfix);
+            Console.WriteLine(postfix);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
